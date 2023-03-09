1e38374b4ce8388632a057f449042436ea441fd0
Rohit9252 <rohitbatra0165@gmail.com>
2023-03-09 16:13:16 +0530

    pagination-added

--- controller/products.js
+++ controller/products.js
@@ -38,16 +38,24 @@ const getAllProducts = async (req, res) => {
         apiData = apiData.select(selectFix);
 
     }
+ 
+    let page  = Number(req.query.page) || 1;
+    let limit = Number(req.query.limit) || 3;
+
+
+    let skip = (page - 1) * limit;
+    apiData = apiData.skip(skip).limit(limit);
+
 
     console.log(queryObject);
     const myData = await apiData;
-    res.status(200).json({ myData });
+    res.status(200).json({ myData , nbHits: myData.length});
 
 };
 const getAllProductsTesting = async (req, res) => {
     const myData = await Product.find(req.query).select("name company");
     console.log(req.query);
-    res.status(200).json({ myData });
+    res.status(200).json({ myData , nbHits: myData.length});
 
 };
 
